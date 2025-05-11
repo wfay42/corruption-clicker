1558f3f3ae1528c19424660d7d6b527cf050ad82
wfay42 <wfay42@gmail.com>
2025-05-10 21:18:38 -0400

    Figured out how to fade between two images using Clipping

--- sprite_worker.gd
+++ sprite_worker.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 
 var _updateSpriteFunc: Callable
 
+var next_level = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_updateSpriteFunc = _update_sprites
@@ -15,12 +17,16 @@ func _update_sprint_noop(unused: float) -> bool:
 	return false
 
 func _update_sprites(new_corruption: float) -> bool:
-	if new_corruption >= 15:
-		texture = preload("res://img/char-arisa-02.png")
+	if new_corruption >= next_level and get_meta("second_phase") == false:
+		#texture = preload("res://img/char-arisa-02.png")
+		var offset: Vector2 = Vector2.DOWN * 20
+		global_translate(offset)
+		get_parent().global_translate(-offset)
+		next_level += 1
 		return true
 	return false
 
 func _on__hidden_value_controller_corruption_changed(new_corruption: Variant) -> void:
-	var retVal = _updateSpriteFunc.call(new_corruption)
-	if retVal:
-		_updateSpriteFunc = _update_sprint_noop
+	_updateSpriteFunc.call(new_corruption)
+	#if retVal:
+		#_updateSpriteFunc = _update_sprint_noop
