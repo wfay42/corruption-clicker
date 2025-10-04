b7d461fe6080fc927969345bb962fc7946bbd22d
wfay42 <wfay42@gmail.com>
2025-10-03 22:17:56 -0400

    Inverted relationship between signals in the buttons and labels, and now doubting if that's right

--- gameplay/rps_button.gd
+++ gameplay/rps_button.gd
@@ -5,11 +5,11 @@ const RPS_CHOSEN_NAME: String = "rps_chosen"
 
 func _ready() -> void:
 	## TODO: should flip this relationship around so YourInput is what says to connect to this signal
-	var yourInput: Node = get_parent().get_node("Countdown").get_node("YourInput")
-	rps_chosen.connect(yourInput._on_rps_button_rps_chosen)
+	# var yourInput: Node = get_parent().get_node("Countdown").get_node("YourInput")
+	# rps_chosen.connect(yourInput._on_rps_button_rps_chosen)
+	pass
 
 
 func _on_pressed() -> void:
-	# var rps_type = self.text
-	var rps_type = "rok"
-	emit_signal(RPS_CHOSEN_NAME, rps_type)
+	var rps_type: String = self.text
+	rps_chosen.emit(rps_type)
--- gameplay/rps_label.gd
+++ gameplay/rps_label.gd
@@ -1,15 +1,12 @@
-extends Label
-
+class_name RPSLabel extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# self.connect(RPSButton.RPS_CHOSEN_NAME, _on_rps_button_rps_chosen)
-	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# wire up all three of the Rock, Paper, and Scissors nodes to this
+	for nodename in ["Rock", "Paper", "Scissors"]:
+		var node = get_parent().get_parent().get_node(nodename)
+		node.rps_chosen.connect(_on_rps_button_rps_chosen)
 
 # Hook up to signal rps_chosen (RPS_CHOSEN_NAME) in rps_button.gd
 func _on_rps_button_rps_chosen(rps: String) -> void:
