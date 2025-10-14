eabfa84f9952d6eb445ccc5aaa1bbc8123ddf6f8
wfay42 <wfay42@gmail.com>
2025-10-13 21:56:20 -0400

    Hooked up _rps_controller and added signal to start timer when rps_chosen

--- gameplay/_rps_controller.gd
+++ gameplay/_rps_controller.gd
@@ -1,7 +1,22 @@
 extends Node
 
+var timer: Timer
+
+func _ready() -> void:
+    timer = Timer.new()
+    timer.one_shot = true
+    timer.timeout.connect(_on_timer_timeout)
+    print("Is timer stopped? %s" % timer.is_stopped())
+
+    # timer must be added to the tree before it can be started
+    self.add_child(timer)
+
 func _process(delta: float) -> void:
     pass
 
-func rps_chosen(asdf: String) -> void:
-    pass
+func _on_rps_chosen(value: String) -> void:
+    if timer.is_stopped():
+        timer.start(3.0)
+
+func _on_timer_timeout() -> void:
+    print("Timer timeout")
--- gameplay/rps_label.gd
+++ gameplay/rps_label.gd
@@ -3,10 +3,16 @@ class_name RPSLabel extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
+	var root: Node = get_parent().get_parent()
+	var controller = root.get_node("RPSController")
+
 	# wire up all three of the Rock, Paper, and Scissors nodes to this
 	for nodename in ["Rock", "Paper", "Scissors"]:
-		var node = get_parent().get_parent().get_node(nodename)
-		node.rps_chosen.connect(_on_rps_button_rps_chosen)
+		var node = root.get_node(nodename)
+
+		if node.has_signal(RPSButton.RPS_CHOSEN_NAME):
+			node.rps_chosen.connect(_on_rps_button_rps_chosen)
+			node.rps_chosen.connect(controller._on_rps_chosen)
 
 # Hook up to signal rps_chosen (RPS_CHOSEN_NAME) in rps_button.gd
 func _on_rps_button_rps_chosen(rps: String) -> void:
