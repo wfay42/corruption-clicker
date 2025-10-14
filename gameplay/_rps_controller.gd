501945ed61b8000df02c7ad37b3f5dff9b14a3d6
wfay42 <wfay42@gmail.com>
2025-10-13 22:07:22 -0400

    Fixing up timer and putting in scaffold for connecting to computer choice

--- gameplay/_rps_controller.gd
+++ gameplay/_rps_controller.gd
@@ -1,22 +1,30 @@
 extends Node
 
 var timer: Timer
+var timer_duration: float
+
+var computerChoice: Node
 
 func _ready() -> void:
-    timer = Timer.new()
-    timer.one_shot = true
-    timer.timeout.connect(_on_timer_timeout)
-    print("Is timer stopped? %s" % timer.is_stopped())
+	timer_duration = 3.0
 
-    # timer must be added to the tree before it can be started
-    self.add_child(timer)
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
+
+	# timer must be added to the tree before it can be started
+	self.add_child(timer)
+
+    # TODO: Need to figure out how to connect this to the computer's choice,
+    # or solve this by emitting a signal and hooking that up elsewhere
+	# computerChoice = self.get_node("Countdown").get_node("YourInput")
 
 func _process(delta: float) -> void:
-    pass
+	pass
 
 func _on_rps_chosen(value: String) -> void:
-    if timer.is_stopped():
-        timer.start(3.0)
+	if timer.is_stopped():
+		timer.start(timer_duration)
 
 func _on_timer_timeout() -> void:
-    print("Timer timeout")
+	print("Timer timeout")
