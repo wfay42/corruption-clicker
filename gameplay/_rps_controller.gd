295f6623a0bed92999ed1da4c9988dc882b81917
wfay42 <wfay42@gmail.com>
2025-10-15 21:51:48 -0400

    Rewired _rps_controller script onto the RockPaperScissors scene, and moved signal connection logic into there

--- gameplay/_rps_controller.gd
+++ gameplay/_rps_controller.gd
@@ -15,9 +15,23 @@ func _ready() -> void:
 	# timer must be added to the tree before it can be started
 	self.add_child(timer)
 
-    # TODO: Need to figure out how to connect this to the computer's choice,
-    # or solve this by emitting a signal and hooking that up elsewhere
-	# computerChoice = self.get_node("Countdown").get_node("YourInput")
+	# TODO: Need to figure out how to connect this to the computer's choice,
+	# or solve this by emitting a signal and hooking that up elsewhere
+	computerChoice = self.get_node("Countdown").get_node("YourInput")
+
+	_connect_children()
+
+func _connect_children() -> void:
+	var controller = self
+	var countdown = self.get_node("Countdown")
+	var yourInput = countdown.get_node("YourInput")
+
+	for rps_nodename in ["Rock", "Paper", "Scissors"]:
+		var rps_node = self.get_node(rps_nodename)
+
+		if rps_node.has_signal(RPSButton.RPS_CHOSEN_NAME):
+			rps_node.rps_chosen.connect(yourInput._on_rps_button_rps_chosen)
+			rps_node.rps_chosen.connect(controller._on_rps_chosen)
 
 func _process(delta: float) -> void:
 	pass
