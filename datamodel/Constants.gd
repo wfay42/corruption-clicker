381fe9d29ffb9b5ab5d1a518db2a99e2fdb9cccc
wfay42 <wfay42@gmail.com>
2026-03-25 10:32:26 -0400

    Changed text colors a bit

--- datamodel/Constants.gd
+++ datamodel/Constants.gd
@@ -8,9 +8,9 @@ const PLAYER_NAME: String = "Player"
 const COMPUTER_NAME: String = "Computer"
 const TIE_NAME: String = "Tie"
 
-const PLAYER_COLOR: Color = Color.GAINSBORO
-const COMPUTER_COLOR: Color = Color.INDIGO
-const TIE_COLOR: Color = Color.GRAY
+const PLAYER_COLOR: Color = Color.YELLOW
+const COMPUTER_COLOR: Color = Color.DARK_RED
+const TIE_COLOR: Color = Color.LIGHT_BLUE
 
 static func get_player_color(result: String) -> Color:
 	if result == PLAYER_NAME:
